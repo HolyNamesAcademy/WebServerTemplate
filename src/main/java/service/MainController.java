fdf55a004b763370201d070b9fab10bf582fe0b8
Harsha Gotur <hgotur@gmail.com>
2021-08-30 08:59:53 -0400

    move index from static to templates

--- src/main/java/service/MainController.java
+++ src/main/java/service/MainController.java
@@ -7,11 +7,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
 
 	@GetMapping("/hello")
 	public String hello(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
 		return "hello";
 	}
-
 }
