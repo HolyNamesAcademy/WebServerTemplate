45ca0906be781b85804a9157dbc924af7fefe532
Harsha Gotur <hgotur@gmail.com>
2022-09-06 20:20:13 -0700

    create a Sql DB class with env vars set up

--- src/main/java/service/MainController.java
+++ src/main/java/service/MainController.java
@@ -1,5 +1,6 @@
 package service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
+	@Autowired
+	private SqlDB sqlDB;
+
 	@GetMapping("/")
 	public String index() {
 		return "index";
---
+++ src/main/java/service/SqlDB.java
@@ -0,0 +1,28 @@
+package service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SqlDB {
+    private String hostName;
+    private final String dbName;
+    private final String user;
+    private final String password;
+    private final String connectionUrl;
+
+    @Autowired
+    public SqlDB(Environment env) {
+        hostName = env.getProperty("db.hostname");
+        dbName = env.getProperty("db.db_name");
+        user = env.getProperty("db.user");
+        password = env.getProperty("db.password");
+
+        connectionUrl = String.format("jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;"
+                + "hostNameInCertificate=*.database.windows.net;loginTimeout=30;", hostName, dbName, user, password);
+    }
+
+    // TODO: Add methods to talk to your DB here
+
+}
