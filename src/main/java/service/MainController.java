1d6b32d44c7cb49da2919dc0d368e7a00225dc38
Harsha Gotur <hgotur@gmail.com>
2022-09-06 17:40:34 -0700

    Revert "demo of new web page"
    This reverts commit de440995ab51e11e066cfd62e693d8a54d68b8f5.

--- src/main/java/service/MainController.java
+++ src/main/java/service/MainController.java
@@ -3,10 +3,7 @@ package service;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.List;
 
 @Controller
 public class MainController {
@@ -19,14 +16,5 @@ public class MainController {
 	public String hello(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
 		return "hello";
-	}
-
-	@GetMapping("/foods/{category}")
-	public String foods(@PathVariable("category") String category, Model model) {
-		List<String> foods = PretendDatabase.getFoodsByCategory(category);
-		model.addAttribute("foods", foods);
-
-		// html page
-		return "foods";
 	}
 }
