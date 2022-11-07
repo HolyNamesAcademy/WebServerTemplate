3b927cec796ca1846b3d44b03cfe2769ca51c70d
KermitHermit121 <112117889+ljackson23@users.noreply.github.com>
2022-11-07 08:53:19 -0800

    Maybe making the upload page work

--- src/main/java/service/MainController.java
+++ src/main/java/service/MainController.java
@@ -4,7 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class MainController {
@@ -19,6 +24,21 @@ public class MainController {
 	@GetMapping("/hello")
 	public String hello(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
-		return "hello";
+		return "hello";}
+	@GetMapping ("/uploadPage")
+	public String uploadPage(Model m){
+		Post post = new Post();
+		m.addAttribute("plant");
+		return "/uploadPage";
+
+		List<String> listStatus = Arrays.asList("Alive", "Dead", "Watered", "Un-Watered");
+		m.addAttribute("listStatus", listStatus);
+
+		return "uploadPage";
+	}
+	@PostMapping("/uploadPage")
+	public String submitUploadPage(@ModelAttribute("post") Post post){
+		System.out.print(post);
+		return "uploadSuccess";
 	}
 }
