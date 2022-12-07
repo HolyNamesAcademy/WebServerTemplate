c9a17d028f61992e03fccaace0ee7205eb24473f
AerialAstrea <elizabethtdau@gmail.com>
2022-12-07 07:26:47 -0800

    :))

--- src/main/java/service/MainController.java
+++ src/main/java/service/MainController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -22,7 +23,9 @@ public class MainController {
 		return "hello";
 	}
 
+	@GetMapping("/upload") // change to post later
 	public void uploadPost() {
+		// Post(int PlantId, int Age, String PlantName, String Species, String Status, String NameOfUser, String Caption, String PhotoUrl)
 		Post test = new Post(600, 16, "Penguins", "Left", "Dead", "Sophia", "Penguins!", "https://flowermag.com/wp-content/uploads/2018/02/shasta-daisies-becky-1000x669.jpg");
 
 		sqlDB.uploadPost(test);
