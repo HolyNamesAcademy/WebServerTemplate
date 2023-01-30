ad57580d61ccba1790d927868d12768f37e1d53d
alanaaaa <ALombaard23@holynames-sea.org>
2023-01-30 07:52:09 -0800

    Stylized feed page (nav bar)

--- src/main/java/service/MainController.java
+++ src/main/java/service/MainController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+
 @Controller
 public class MainController {
 	@Autowired
@@ -35,13 +37,22 @@ public class MainController {
 	HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
 	public respHttpResponse<String> getResponse() {return response;}
 	 */
-	
+
 	@GetMapping("/upload") // change to post later
 	public String uploadPost() {
 		// Post(int PlantId, int Age, String PlantName, String Species, String Status, String NameOfUser, String Caption, String PhotoUrl)
 		Post test = new Post(459, 987, "hi", "kjhgfd", "jgyhtg", "Sophia", "Penguins!", "https://flowermag.com/wp-content/uploads/2018/02/shasta-daisies-becky-1000x669.jpg");
 
-		
+	@GetMapping("/feed")
+	public String feed(@RequestParam(name="Clarence", required=false, defaultValue="planty") String name, Model model){
+		model.addAttribute("Clarence", name);
+		Post plant = new Post(01, 01, 00, "plantbert", "green", "not green", "Alana", "pretty dead", "dfdhsjkf");
+		ArrayList<Post> posts = new ArrayList<>();
+		posts.add(plant);
+		model.addAttribute(posts);
+		return "/feed";
+	}
+
 		sqlDB.uploadPost(test);
 		return "index";
 	}
