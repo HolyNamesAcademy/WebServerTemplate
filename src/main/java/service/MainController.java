009113291e35f73e2734173612e25358e6b7eb81
alanaaaa <ALombaard23@holynames-sea.org>
2023-03-08 07:50:47 -0800

    fixed errors and got access to feed page

--- src/main/java/service/MainController.java
+++ src/main/java/service/MainController.java
@@ -64,7 +64,7 @@ public class MainController {
 	public respHttpResponse<String> getResponse() {return response;}
 	 */
 
-
+/*
 	@GetMapping("/feed")
 	public String feed(@RequestParam(name="Clarence", required=false, defaultValue="planty") String name, Model model){
 		model.addAttribute("Clarence", name);
@@ -72,30 +72,43 @@ public class MainController {
 		Post plant2 = new Post(02, 02, 00, "Belle-adona", "~~blue belle*&^$#@", "livin' life to the fullest", "Alana", "IM BLUE`*%@^!", "https://www.woodlandtrust.org.uk/media/4272/bluebells-close-up-wtml-1024791-web-upload.jpg");
 
 		ArrayList<Post> posts = new ArrayList<>();
-		posts.add(plant);
+		posts.add(plant1);
+		posts.add(plant2);
 		model.addAttribute(posts);
 
+
 		ArrayList<Post> posts = new ArrayList<Post>();
-		posts = sqlDB.viewPosts();
+		//posts = sqlDB.viewPosts();
 		posts.add(plant1);
 		posts.add(plant2);
 		model.addAttribute("posts", posts);
-		return "/feed";
+
+		//return "/feed";
 	}
 
+ */
 
 
-	/*
+
 	@GetMapping("/feed") // Elizabeth's testing for viewPost (not working)
 	public String feed(@RequestParam(name="Clarence", required=false, defaultValue="planty") String name, Model model){
 		model.addAttribute("Clarence", name);
-		TreeMap<Integer, Post> post = sqlDB.viewPosts();
+
+		Post plant1 = new Post(01, 01, 00, "plantbert", "green", "not green", "Alana", "pretty dead", "https://asset.bloomnation.com/c_pad,d_vendor:global:catalog:product:image.png,f_auto,fl_preserve_transparency,q_auto/v1627197230/vendor/2864/catalog/product/2/0/20200304122155_file_5e5ef4a3ccb60_5e5ef7b7cd5fa_600b664eac11a._600b6650be20c..jpg");
+		Post plant2 = new Post(02, 02, 00, "Belle-adona", "~~blue belle*&^$#@", "livin' life to the fullest", "Alana", "IM BLUE`*%@^!", "https://www.woodlandtrust.org.uk/media/4272/bluebells-close-up-wtml-1024791-web-upload.jpg");
 
 		ArrayList<Post> posts = new ArrayList<>();
-		for (int i = 0; i < 1; i++) {
+		posts.add(plant1);
+		posts.add(plant2);
+		model.addAttribute(posts);
+
+		TreeMap<Integer, Post> post = sqlDB.viewPosts();
+		for (int i = 1; i < 2; i++) {// the starting postID value in the database is 1 (lowest value there)
 			Post plant = post.get(i);
 			posts.add(plant);
-			System.out.println(plant.PlantName, plant.PhotoUrl, plant.Caption);
+			System.out.print(plant.PlantName);
+			System.out.print(plant.PhotoUrl);
+			System.out.println(plant.Caption);
 		}
 
 		model.addAttribute("posts", posts);
@@ -117,5 +130,5 @@ public class MainController {
 			System.out.println(p);
 		}
 		return "index";
-	} */
+	}
 }
