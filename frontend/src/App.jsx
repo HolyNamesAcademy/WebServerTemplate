454115b6db8d9bf18d24417e8f711c8f4cd38cc5
Alexander Bell-Towne <alexander@bell-towne.com>
2023-12-11 23:09:20 -0800

    Setup basic routing structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,27 @@
-import Container from '@mui/material/Container';
-import Hello from './components/Hello';
+import { Routes, Route } from 'react-router-dom';
+import DefaultLayout from './layouts/DefaultLayout';
+import HomePage from './pages/HomePage';
+import OtherPage from './pages/OtherPage';
+import NoMatchPage from './pages/NoMatchPage';
 
 function App() {
   return (
-    <Container maxWidth="sm">
-      <Hello />
-    </Container>
+    <div>
+      {/* Routes nest inside one another. Nested route paths build upon
+      parent route paths, and nested route elements render inside
+      parent route elements. See the note about <Outlet> in DefaultLayout. */}
+      <Routes>
+        <Route path="/" element={<DefaultLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/other" element={<OtherPage />} />
+
+          {/* Using path="*"" means "match anything", so this route
+                acts like a catch-all for URLs that we don't have explicit
+                routes for. */}
+          <Route path="*" element={<NoMatchPage />} />
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import Hello from '../components/Hello';
+
+// This is the home page component. Change it to make
+// changes to the home page.
+function HomePage() {
+  return (
+    // You can put other components here.
+    <Hello />
+  );
+}
+
+export default HomePage;
