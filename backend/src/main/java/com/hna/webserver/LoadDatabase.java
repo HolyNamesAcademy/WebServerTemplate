HolyNamesAcademy/Projects-II-23-24-TTh

I have committed the user class.

backend/src/main/java/com/hna/webserver/LoadDatabase.java
@@ -15,8 +15,9 @@ public class LoadDatabase {
     @Bean
     CommandLineRunner initDatabase(UserRepository repository) {
         return args -> {
-            log.info("Preloading " + repository.save(new User("Bilbo Baggins")));
-            log.info("Preloading " + repository.save(new User("Frodo Baggins")));
+            log.info("Preloading " + repository.save(new User("Bilbo Baggins", "BBaggins@holynames-sea.org", "bilba")));
+            log.info("Preloading " + repository.save(new User("Frodo Baggins", "FBaggins@holynames-sea.org", "froba")));
+            log.info("Preloading " + repository.save(new User("Gandalf Baggins", "GBaggins@holynames-sea.org", "ganba")));
         };
     }
 }

HolyNamesAcademy/WebServerTemplate

backend/src/main/java/com/hna/webserver/LoadDatabase.java
@@ -17,8 +17,9 @@ public class LoadDatabase {
     @Bean
     CommandLineRunner initDatabase(UserRepository userRepository, CharbyRepository charbyRepository) {
         return args -> {
-            log.info("Preloading " + userRepository.save(new User("Bilbo Baggins")));
-            log.info("Preloading " + userRepository.save(new User("Frodo Baggins")));
+            log.info("Preloading " + userRepository.save(new User("Bilbo Baggins", "BBaggins@holynames-sea.org", "bilba")));
+            log.info("Preloading " + userRepository.save(new User("Frodo Baggins", "FBaggins@holynames-sea.org", "froba")));
+            log.info("Preloading " + userRepository.save(new User("Gandalf Baggins", "GBaggins@holynames-sea.org", "ganba")));
             log.info("Preloading " + charbyRepository.save(new Charby("Charby 1")));
         };
     }